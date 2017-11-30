eb9560b16045ce290e6ed1c9e1da623b493c7712
Aruna Amaresan <aruna.amaresan@gmail.com>
2017-11-30 06:25:48 -0500

    Creating files and directory structure for work

---
+++ PyBank/main.py
@@ -0,0 +1,21 @@
+# Test Start 
+# 0 to 9 loop
+for x in range(10)
+	print x
+
+# start from 20 to 29
+for x in range(20,30)
+	print x
+
+# you wont need a loop if you want to print  together or store in another variable. 
+for x in ["peanut", "butter"]
+	print(x) 
+
+#Pythn is case sensitive when string compare 
+
+run = "Yes" 
+while run == "yes"
+	print("Whee Message...")
+	run = input("Would you like to continue on merry go around ? ")
+
+
